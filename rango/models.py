d3d5bb5a78e171f6745db1fe2d40b44c91836a62
Natasha Graham <2636245g@student.gla.ac.uk>
2024-01-26 17:39:04 +0000

    5.8

--- rango/models.py
+++ rango/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(0)
-    likes = models.IntegerField(0)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -11,7 +11,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-
+    
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
